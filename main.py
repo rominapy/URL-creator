63d58255e221e050e45dbafea5d370241b15e250
Romina Pouya <148919045+rominapy@users.noreply.github.com>
2024-05-21 19:13:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyqrcode
+from pyqrcode import QRCode
+
+# String which represent the QR code
+s = "https://github.com/rominapy"
+
+# Generate QR code
+
+url = pyqrcode.create(s)
+
+# Create and save the png file naming "myqr.png"
+url.svg("myQr.svg", scale = 8)
